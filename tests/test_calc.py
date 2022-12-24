aebe5aaa956524e4e8f5d11e38d2902f7d29b50e
ilimsk <113999486+ilimsk@users.noreply.github.com>
2022-12-24 19:45:09 +0700

    new

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -10,12 +10,15 @@ class TestCalc:
     def test_adding_success(self):
         assert self.calc.adding(self, 1, 1) == 2
 
-    def test_adding_unsuccess(self):
-        assert self.calc.adding(self, 1, 1) == 3
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 3) == 6
 
     def test_zero_division(self):
         with pytest.raises(ZeroDivisionError):
             self.calc.division(self, 1, 0)
+
+    def test_multiply_subtraction(self):
+        assert self.calc.subtraction(self, 5, 3) == 2
 
     def teardown(self):
         print('Выполнение метода Teardown')
